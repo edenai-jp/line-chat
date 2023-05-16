23384081f2be2f39a96acc47c5b772c60ba7f457
xiaohuachen <tinsyouka@gmail.com>
2023-05-16 11:32:17 +0000

    add line users table

--- database/migrations/2023_05_16_113020_create_line_users_table.php
+++ database/migrations/2023_05_16_113020_create_line_users_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('line_users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
-            $table->string('password');
             $table->string('line_id');
-            $table->rememberToken();
             $table->string('token');
             $table->timestamps();
-            $table->timestamp('email_verified_at')->nullable();
             $table->softDeletes();
         });
     }
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('line_users');
     }
 };
